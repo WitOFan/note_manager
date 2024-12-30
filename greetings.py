3fa1182378d33db6815ec17165061f9894bae063
WitOFan <andru31ru@yandex.ru>
2024-12-30 19:28:16 +0300

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -6,8 +6,11 @@ status = 'status of the note'
 created_date = '01.01.1970'
 issue_date = '19.01.2038'
 
-print("Имя пользователя:", username, "\nЗаголовок заметки:", title,
-      "\nОписание заметки:", content, "\nСтатус заметки:", status,
-      "\nДата создания заметки:", created_date, "\nДата истечения заметки:", issue_date)
 
+def print_data():
+      print("Имя пользователя:", username, "\nЗаголовок заметки:", title,
+            "\nОписание заметки:", content, "\nСтатус заметки:", status,
+            "\nДата создания заметки:", created_date, "\nДата истечения заметки:", issue_date)
 
+if __name__ == '__main__':
+    print_data()
