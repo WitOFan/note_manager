e60d3f256d274c2fab43ee02aefe59491fc06a43
WitOFan <andru31ru@yandex.ru>
2024-12-30 19:28:54 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,7 @@
+from greetings import created_date, issue_date
+
+temp_issue_date = issue_date[0:5]
+temp_created_date = created_date[0:5]
+
+print(f"Дата создания заметки: {temp_created_date}\n"
+      f"Дата истечения заметки: {temp_issue_date}")
