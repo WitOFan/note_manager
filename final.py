1b8ed729a462972798415243c762ee62622c582b
WitOFan <andru31ru@yandex.ru>
2025-01-03 14:20:39 +0300

    Add files via upload

--- final.py
+++ final.py
@@ -1,6 +1,6 @@
-#Список для всего
+#Список, как было сказано в задании 5
 
-note = []
+'''note = []
 
 note.append(input("Введите имя пользователя: "))
 note.append(input("Введите основной текст заметки: "))
@@ -17,4 +17,26 @@ title_list.append(input("Введите третий заголовок заме
 note.append(title_list)
 
 for i in note:
-    print(i)
+    print(i)'''
+
+#Словарь, как сказано в завершении этапа
+
+note = {
+    'username': input("Введите имя пользователя: "),
+    'content': input("Введите основной текст заметки: "),
+    'status': input("Введите статус заметки: "),
+    'created_date': input("Введите дату написания заметки в формате дд.мм.гггг: "),
+    'issue_date': input("Введите дату завершения заметки (дедлайн) в формате дд.мм.гггг: ")
+}
+
+title_list =[]
+
+title_list.append(input("Введите первый заголовок заметки: "))
+title_list.append(input("Введите второй заголовок заметки: "))
+title_list.append(input("Введите третий заголовок заметки: "))
+
+note ['titles'] = title_list
+
+for i in note:
+    print(f'{i}: {note[i]}')
+
