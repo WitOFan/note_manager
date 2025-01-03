82999720c1454e188f1b53e3a30a2e603f93e962
WitOFan <andru31ru@yandex.ru>
2025-01-03 13:50:21 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -3,6 +3,7 @@
 def date_changer_func (data):
       return data[0:5]
 
+#Функция для вывода принта только при исполнении этого файла напрямую
 def print_test():
       print(date_changer_func('19.01.2038'))
 
