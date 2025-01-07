eb14889897d980939de72b4e87eef8be0bf8e2ef
WitOFan <andru31ru@yandex.ru>
2025-01-07 19:40:34 +0300

    Add files via upload

--- check_deadline.py
+++ check_deadline.py
@@ -1,7 +1,6 @@
 #сравнение с текущей датой
 
 import datetime
-import re
 
 current_dt = datetime.date.today()                                                                  # получение текущей даты
 current_dt_str = current_dt.strftime('%d-%m-%Y')                                                    # и преобразование её в строку
@@ -22,7 +21,7 @@ while loop:                                                                     
         continue
 
 delta = issue_date_format - current_dt                                                              # нахождение разницы дат
-delta_str = re.search('[-+]?\d+', str(delta))[0]                                             # выделение дней из timedelta
+delta_str = str(delta).split()[0]                                                                   # выделение дней из timedelta
 if (int(delta_str[-1]) == 1) and (int(delta_str) != 11):                                            # определение день/дня/дней
     day = 'день'
 elif (1 < int(delta_str[-1]) < 5) and ((10 > int(delta_str)) or int(delta_str) > 20):
