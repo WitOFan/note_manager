fec1056d37242bc2e63df49fc4f1e56e20470b9c
Andrey <andru31ru@yandex.ru>
2025-01-14 13:19:16 +0300

    Задание 4, этап 3. Функция поиска заметок

---
+++ search_notes_function.py
@@ -0,0 +1,61 @@
+import json
+
+with open("data_file.json", 'r') as json_file:                                                                      # Чтение заметок из json
+    note_list = json.load(json_file)
+
+def search_notes(notes, keyword=None, status=None):
+    search_list = []
+
+    for note_el in notes:                                                                                           # Цикл поиска
+        if (type(keyword) == str) and (type(status) == str):                                                        # Поиск по ключевому слову и статусу
+            if note_el["Статус"].lower() == status.lower():
+                for i in note_el:
+                    if keyword.lower() in note_el[i].lower():
+                        search_list.append(note_el)
+                        break
+        elif type(keyword) == str:                                                                                  # Поиск по ключевому слову
+            for i in note_el:
+                if keyword.lower() in note_el[i].lower():
+                    search_list.append(note_el)
+                    break
+        elif type(status) == str:                                                                                   # Поиск по статусу
+            if note_el["Статус"].lower() == status.lower():
+                search_list.append(note_el)
+
+    if len(search_list) == 0:                                                                                       # Если ничего не найдено
+        print('Подходящих заметок не найдено')
+    else:                                                                                                           # Если поиск успешный
+        count = 1
+        print('Найденные заметки:')
+        for search_el in search_list:  # Вывод заметок
+            print(f'\nЗаметка {count}')
+            for j in search_el:
+                print(f'{j}: {search_el[j]}')
+            print('------------------------------------')
+            count = count + 1
+
+while True:
+    print('Выбирете способ посика:\n'
+          '1. Поиск по ключевому слову\n'
+          '2. Поиск по статусу\n'
+          '3. Поиск по ключевому слову и статусу\n')
+    user_choice = input('Введите число: ')
+    if user_choice == '1':                                                                                          # Поиск по ключевому слову
+        user_keyword = input('Введите ключевое слово для поиска: ')
+        search_notes(note_list, user_keyword.strip())
+    elif user_choice == '2':                                                                                        # Поиск по статусу
+        user_status = input('Введите статус заметки для поиска: ')
+        search_notes(note_list, status=user_status.strip())
+    elif user_choice == '3':                                                                                        # Поиск по ключевому слову и статусу
+        user_keyword = input('Введите ключевое слово для поиска: ')
+        user_status = input('Введите статус заметки для поиска: ')
+        search_notes(note_list, user_keyword.strip(), user_status.strip())
+    else:                                                                                                           # Некорректный ввод
+        print('Пожалуйста, введите число, обозначающее вариант поиска (1, 2, 3)')
+        continue
+    repeat_no = input('Вы хотите повторить поиск? (да/нет): ')                                                      # Запрос на повторный поиск
+    if (repeat_no == 'д') or (repeat_no == 'да'):
+        continue
+    elif (repeat_no == 'н') or (repeat_no == 'не') or (repeat_no == 'нет'):
+        break
+
