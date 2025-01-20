f0f91456dbbd91a432cd666c49ee1e4251783d42
Andrey <andru31ru@yandex.ru>
2025-01-20 22:11:06 +0300

    Этап 4 Задание 1

---
+++ save_notes_to_file.py
@@ -0,0 +1,11 @@
+import json
+
+def save_notes_to_file(notes, filename):
+    with open(f"{filename}.json", 'w') as write_file:
+        json.dump(notes, write_file, ensure_ascii=False)
+
+if __name__ == '__main__':
+    with open("data_file.json", 'r') as json_file:  # Чтение заметок из json
+        note_list = json.load(json_file)
+
+    save_notes_to_file(note_list, "Notes")
